5e2ac7b93f826e1b1899abec2bd4256a53775f2e
mehmetozturan <mehmetozturan.proton.me>
2024-04-02 11:52:45 +0300

    pass bt pointers

--- 6.functions/pass_by_pointer.cpp
+++ 6.functions/pass_by_pointer.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-void say_age(int* age); // Declaration
+void say_age(int* age); //* Declaration Pass by pointer
+void say_age(const int* age); //* Declaration Pass by pointer to const
+
+int dog_count{2};
+
+void say_age(const int* const age); //* Declaration Pass by const pointer to const
 
 int main(){
 
@@ -12,8 +17,18 @@ int main(){
     return 0;
 }
 
-
-void say_age(int* age){ // Parameter
+void say_age(int* age){ // Pass by pointer
     ++(*age); 
     std::cout <<  "Hello , you are " << *age << " years old! &age : " << &age <<  std::endl;//24
+}
+
+void say_age(const int* age){ // Pass by pointer to const
+    //++(*age); //
+    std::cout <<  "Hello , you are " << *age << " years old! &age : " << &age <<  std::endl;//24
+}
+
+void say_age(const int* const age){ // Pass by const pointer to const
+    //++(*age); //
+    std::cout <<  "Hello , you are " << *age << " years old! &age : " << &age <<  std::endl;//24
+    //age = & dog_count;
 }
